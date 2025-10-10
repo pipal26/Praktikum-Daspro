a0593ca706fe9e2b7205ac7be19dc7e73912a202
pipal26 <novalgg87@gmail.com>
2025-10-10 11:18:55 +0700

    Variabel UKT lunas boolean

---
+++ JOBSHEET5/ifCetakKRS14.java
@@ -0,0 +1,13 @@
+package JOBSHEET5;
+
+import java.util.Scanner;
+
+public class ifCetakKRS14 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    boolean uktLunas;
+    System.out.println("--- Cetak KRS SIAKAD ---");
+    System.out.println("Apakah UKT sudah lunas? (true/false)");
+    uktLunas = sc.nextBoolean();
+    }
+}
